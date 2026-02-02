4242a721ae1042e9bf33e59f360420a8094c4fbb
shahriarc224 <145952063+shahriarc224@users.noreply.github.com>
2026-02-02 10:56:22 +0600

    Refactor Meme annotation tool for clarity and structure

--- Meme_annotator_Prejudice (1).py
+++ Meme_annotator_Prejudice (1).py
@@ -7,7 +7,7 @@ from tkinter import messagebox
 image_folder = r"E:\meme resesarch by sabbir\Memes (2)(1)\Memes"
 excel_path = r"E:\meme resesarch by sabbir\Memes (2)(1)\Memes\Meme_annotations_Binar.xlsx"
 target_column = "Prejudice"
-BATCH_SIZE = 6     # You mentioned 4 in your text, but your code uses 6. I kept 6 to match your grid
+BATCH_SIZE = 6     # You mentioned 4 in your text, but your code uses 6. I kept 6 to match your grid....
 
 
 df = pd.read_excel(excel_path)
@@ -180,6 +180,7 @@ tk.Button(
 
 load_batch()
 root.mainloop()
+
 
 
 
