7cfa66281d66a80729c978a2bbeecd16201d111d
Md. Shahriar Chowdhury <145952063+shahriarc224@users.noreply.github.com>
2026-07-29 23:56:02 +0600

    Update Meme_annotator_Objectification (1).py

--- Meme_annotator_Objectification (1).py
+++ Meme_annotator_Objectification (1).py
@@ -14,7 +14,6 @@ values = [0, 1]
 IMAGES_PER_PAGE = 8
 COLS = 4
 
-
 # ---------- LOAD DATA ----------
 if not os.path.exists(excel_path):
     print(f"Error: Could not find Excel file at {excel_path}")
